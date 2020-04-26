3d514324e146bffc13b20f59e3790655f83cca1f
Giri-Pai <giri@gaindecktechnologies.com>
2020-04-26 21:59:22 +0530

    Custom Error Handling Mongoose Middleware

---
+++ utils/geocoder.js
@@ -0,0 +1,12 @@
+const NodeGeocoder = require("node-geocoder");
+
+var options = {
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: "https",
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null,
+};
+
+const geocoder = NodeGeocoder(options);
+
+module.exports = geocoder;
