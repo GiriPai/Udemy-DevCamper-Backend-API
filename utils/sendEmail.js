b82880396f34eefa23957037bc707700b492d071
Giri-Pai <giri@gaindecktechnologies.com>
2020-05-04 17:05:08 +0530

    Forget Passoword Impl

---
+++ utils/sendEmail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  // send mail with defined transport object
+  const message = {
+    from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.message, // plain text body
+  };
+
+  const info = await transporter.sendMail(message);
+
+  console.log("Message sent: %s", info.messageId);
+};
+
+module.exports = sendEmail;
