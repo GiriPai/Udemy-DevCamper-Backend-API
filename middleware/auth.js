069d39b9287db34ae56648a5bb16ea7589abfa69
Giri-Pai <giri@gaindecktechnologies.com>
2020-05-04 19:43:24 +0530

    Logout Cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,15 +12,13 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+  } else if (req.cookies.token) {
+    token = req.cookies.token;
   }
-
-  // else if(req.cookies.token){
-  //   token = req.cookies.token
-  // }
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 
   try {
@@ -33,7 +31,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next();
   } catch (err) {
-    return next(new ErrorResponse("Not authorize to access this route", 401));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const {
   resetPassword,
   updateDetails,
   updatePassword,
+  logout,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -15,6 +16,7 @@ const { protect } = require("../middleware/auth");
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/logout", logout);
 router.get("/me", protect, getMe);
 router.put("/updatedetails", protect, updateDetails);
 router.put("/updatepassword", protect, updatePassword);
